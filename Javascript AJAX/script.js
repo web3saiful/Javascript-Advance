7eee25b72f3f5fb22b524292dab06732af232324
web3saiful <web3saiful@gmail.com>
2025-10-29 01:20:46 +0600

    Updated files

---
+++ Javascript AJAX/script.js
@@ -0,0 +1,15 @@
+function loadData(){
+    const xhr=new XMLHttpRequest();
+
+
+    xhr.onload=function(){
+        const container=document.getElementById("demo");
+        demo.innerHTML=xhr.responseText;
+    };
+
+    xhr.open("GET", "./data/data.txt");
+
+    xhr.send();
+}
+
+
---
+++ Javascript API/Geolocation/script.js
@@ -0,0 +1,19 @@
+const display=document.getElementById("display");
+
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }else{
+        display.innerHTML=("Geolocation is not available in your browser!");
+        }
+}
+
+
+function showPosition(position){
+    display.innerHTML=
+    "Latitude: "+
+    position.coords.latitude+
+    "<br/>Longitude: "+
+    Position.coords.longitude
+} 
